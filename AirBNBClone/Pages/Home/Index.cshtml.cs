4c891e13c66ba8178cc0f742ed06c0cceebfb51d
Tanner Cooper <Tannerccooper@gmail.com>
2024-08-12 12:27:39 +0300

    Test GitHub Project

---
+++ AirBNBClone/Pages/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AirBNBClone.Pages.Home
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
