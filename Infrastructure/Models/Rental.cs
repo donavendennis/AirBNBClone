3d1fabb1776438650d3b180ba6682283781681b2
Evan Chan <evanchan040511@gmail.com>
2024-08-12 19:34:05 +0800

    Works now, no compile error

--- Infrastructure/Models/Rental.cs
+++ Infrastructure/Models/Rental.cs
@@ -8,17 +8,17 @@ namespace Infrastructure.Models
         [Key]
         public int Id { get; set; }
 
-        public int Name { get; set; }
+        public int State { get; set; }
 
-        public int Description { get; set; }
+        public int Zip { get; set; }
 
-        public int Name { get; set; }
+        public int City { get; set; }
 
-        public int Description { get; set; }
+        public int Address { get; set; }
 
-        public int Name { get; set; }
+        public int Phone { get; set; }
 
-        public int Description { get; set; }
+        public int OwnerId { get; set; }
 
     }
 }
