14340c7392a5a7f2f9a013f908321d86014bb586
Donaven Dennis <119719733+donavendennis@users.noreply.github.com>
2024-08-14 12:27:31 +0300

    My Rental Page Started

--- AirBNBClone/Pages/Rentals/Index.cshtml.cs
+++ AirBNBClone/Pages/Rentals/Index.cshtml.cs
@@ -13,38 +13,12 @@ namespace AirBNBClone.Pages.Rentals
         public IndexModel(UnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
-            objFeeList = new List<Fee>();
-            objFeeRentalList = new List<FeeRental>();
-            objFeeAmountList = new List<int>();
-            objAmenityList = new List<Amenity>();
-            objRentalAmenityList = new List<RentalAmenity>();
+            objRentalList = new List<Rental>();
         }
-
-        public Rental objRental;
-        public List<Fee> objFeeList;
-        public List<FeeRental> objFeeRentalList;
-        public List<int> objFeeAmountList;
-
-        public List<Amenity> objAmenityList;
-        public List<RentalAmenity> objRentalAmenityList;
 
         public IActionResult OnGet(int id)
         {
-            objRental = _unitOfWork.Rental.GetById(id);
-            objFeeRentalList = _unitOfWork.FeeRental.GetAll().Where(x => x.RentalId == id).ToList();
-            // go through the filteres FeeRental and get the Fee for each one, putting it in objFeeList
-            foreach (var feeRental in objFeeRentalList)
-            {
-                objFeeList.Add(_unitOfWork.Fee.GetById(feeRental.FeeId));
-                objFeeAmountList.Add(feeRental.Amount);
-            }
-
-            objRentalAmenityList = _unitOfWork.RentalAmenity.GetAll().Where(x => x.RentalId == id).ToList();
-            // go through the filteres RentalAmenity and get the Amenity for each one, putting it in objAmenityList
-            foreach (var rentalAmenity in objRentalAmenityList)
-            {
-                objAmenityList.Add(_unitOfWork.Amenity.GetById(rentalAmenity.AmenityId));
-            }
+            objRentalList = _unitOfWork.Rental.GetAll();
             return Page();
         }
 
