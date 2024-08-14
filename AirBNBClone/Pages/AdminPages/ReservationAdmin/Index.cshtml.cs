3dd3b62925f671af66a2ad8fe2afcf890e93f9b5
Tanner Cooper <Tannerccooper@gmail.com>
2024-08-14 21:40:31 +0300

    Created and simple stlyes to reservation admin

---
+++ AirBNBClone/Pages/AdminPages/ReservationAdmin/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Infrastructure.Models;
+using DataAccess;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Infrastructure.Interfaces;
+
+namespace AirBNBClone.Pages.AdminPages.ReservationAdmin
+{
+    public class IndexModel : PageModel
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public IndexModel(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IList<Reservation> Reservations { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Reservations = (await _unitOfWork.Reservation.GetAllAsync()).ToList();
+        }
+    }
+}
